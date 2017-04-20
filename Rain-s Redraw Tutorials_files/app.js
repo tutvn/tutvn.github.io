f471abd3985fc5610b4295d04e16ce8d153adf92
tutvn <shokugekinosoumadst@gmail.com>
2017-04-20 10:13:01 +0700

    Add files via upload

---
+++ Rain-s Redraw Tutorials_files/app.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 23:07:53 thg 3 15, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:53:54 thg 1 5, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+$(function(){
+	var url = document.URL;
+	if(url == "/web/20150315230753/http://www.redhawkscans.com/content.php"){
+		$('#breadcrumb').hide()
+	}
+})
